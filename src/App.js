39ee30a3712c2050dcf8d7a1aba68b2a3a81407d
mdox98 <mdobsurn1@gmail.com>
2022-10-19 11:12:48 -0600

    Basixcs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React , { Component } from "react"
+
+class PlayerDisplay extends React.Component{
+    constructor(props){
+      super(props);
+
+      this.state = {
+      name:"Player 1",
+      hp:10,
+      hpmax:10,
+      armor:3,
+      dmgRange: 6,
+      dmgMin: 1};
+
+      this.updateInfo = this.updateInfo.bind(this);
+      
+    }
+
+    updateInfo(obj){
+      this.setState(obj);
+    }
+
+    render(){
+      return (
+        <>
+        <h1>{this.state.name}</h1>
+        <h3>{"hp: " + this.state.hp + "/" + this.state.hpmax}</h3>
+        <h3>{"dmg: " + this.state.dmgMin + "-" + (this.state.dmgMin+this.state.dmgRange-1)}</h3>
+        <h3>{"armor: " + this.state.armor}</h3>
+        </>
+      );
+    }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PlayerDisplay/>
   );
 }
 
